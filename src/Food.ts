69a0e43d66e042b32b92549fd37ca53f15a9c6bf
TAlam <tanvir.alam.shawn@gmail.com>
2022-10-16 01:46:32 +0200

    Fail/Pass: getName and getUnit returns string

--- src/Food.ts
+++ src/Food.ts
@@ -1,5 +1,18 @@
 class Food {
+    constructor(
+        private readonly name: string,
+        private readonly unit: string,
+    ) {
 
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    getUnit(): string {
+        return this.unit;
+    }
 }
 
 export default Food;
